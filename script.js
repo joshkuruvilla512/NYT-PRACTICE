1761b6d6c9e769bbf804e23b760af1790f81f4a2
Z Naddaf <znaddaf@protonmail.com>
2019-12-20 13:16:22 -0600

    Added the Search box and some features such as a drop down and some variables

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 getArticleInfo("election", 1, 2017, 2017);
+
+// Event Listener
+$("#searchBtn").on("click", function (event) {
+
+    // Prevent default on button click
+    event.preventDefault();
+
+    // Here we grab the input from the input boxes
+    var searchTerm = $("#searchTerm").val();
+    var searchNumber = $("#searchNumber").val();
+    var start = $("#start").val();
+    var end = $("#end").val();
+
+    // Query starts here
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        $("#movie-view").text(JSON.stringify(response));
+    });
