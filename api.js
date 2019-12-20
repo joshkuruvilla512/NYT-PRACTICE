b8f0e813f460558d741a7e13f717a4932ea216d3
Kevin Geary <kgeary@gmail.com>
2019-12-20 13:40:52 -0600

    API working

--- api.js
+++ api.js
@@ -16,9 +16,9 @@ function getArticleInfo(search, limit, startYear, endYear) {
     let startDate = startYear + "0101";
     let endDate = endYear + "1231";
 
-    let API_KEY="Gd5eoflEUsh86GChsCBNO8I0U4GbTzWE";
-    let queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" 
-        + search.replace(" ", "+") 
+    let API_KEY = "Gd5eoflEUsh86GChsCBNO8I0U4GbTzWE";
+    let queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+        + search.replace(" ", "+")
         + "&begin_date=" + startDate
         + "&end_date=" + endDate
         + "&api-key=" + API_KEY;
@@ -26,22 +26,27 @@ function getArticleInfo(search, limit, startYear, endYear) {
     $.ajax({
         url: queryUrl,
         method: "GET",
-    }).then(function(response) {
+    }).then(function (response) {
         // Parse response here
         console.log(response);
         let results = response.response.docs;
-        let ret = [];
-        for(let i=0; i<results.length; i++) {
-            ret.push({
-                headline:   results[i].headline.main,
-                author:     results[i].byline.original,
-                url:        results[i].web_url,
-            });      
-            // Break the loop once we have enough results;
-            if (ret.length == limit) {
-                break;
-            }
+        let articleDiv = $(".topArticles");
+        articleDiv.empty();
+        for (let i = 0; i < limit; i++) {
+            console.log("Creating result", i);
+            let headline = results[i].headline.main;
+            let by = results[i].byline.original;
+            // let url = results[i].web_url;
+            let resultDiv = $("<div>").addClass("result");
+            let titleRow = $("<div>").addClass("title-row");
+            let index = $("<div>").addClass("index").text(i + 1);
+            let title = $("<div>").addClass("title").text(headline);
+            let author = $("<div>").addClass("author").text(by);
+            titleRow.append(index);
+            titleRow.append(title);
+            resultDiv.append(titleRow);
+            resultDiv.append(author);
+            articleDiv.append(resultDiv);
         }
-        return ret;
     });
 }
--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 
 // Get Result Data and update the HTML
 function setResults(searchTerm, numResults, startYear, endYear) {
-    let results = getArticleInfo(searchTerm, numResults, startYear, endYear);
-
-    // Update the HTML with the result Data from
-    let articleDiv = $("#resultDiv");
-    articleDiv.empty();
-    for (let i=0; i<results.length; i++) {
-        let resultDiv = $("<div>").addClass("result");
-        let titleRow = $("<div>").addClass("title-row");
-        let index = $("<div>").addClass("index").text(i+1);
-        let title = $("<div>").addClass("title").text(results[i].headline);
-        let author = $("<div>").addClass("author").text(results[i].author);
-        titleRow.append(index);
-        titleRow.append(title);
-        resultDiv.append(titleRow);
-        resultDiv.append(author);
-        articleDiv.append(resultDiv);
-    }
+    getArticleInfo(searchTerm, parseInt(numResults), parseInt(startYear), parseInt(endYear));
 }
 
 // Event Listener
