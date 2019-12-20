b2a7606c89bd850bd3cdd4c10da6d654eb2e2bbd
Kevin Geary <kgeary@gmail.com>
2019-12-20 17:07:52 -0600

    Styled results and form

--- assets/api.js
+++ assets/api.js
@@ -3,16 +3,22 @@
 // startYear: Year to start looking for articles
 // endYear: 
 function getArticleInfo(search, limit, startYear, endYear) {
+    // If limit not specified set it to default
     if (!limit) {
         limit = 1;
     }
+
+    // If start year not specified set it to default
     if (!startYear) {
         startYear = 2019;
     }
+
+    // If end year not specified set it to default
     if (!endYear) {
         endYear = 2019;
     }
 
+    // Append MMDD to years to get proper date format for NYT
     let startDate = startYear + "0101";
     let endDate = endYear + "1231";
 
@@ -32,21 +38,26 @@ function getArticleInfo(search, limit, startYear, endYear) {
         let results = response.response.docs;
         let articleDiv = $(".topArticles");
         articleDiv.empty();
+
         for (let i = 0; i < limit; i++) {
-            console.log("Creating result", i);
-            let headline = results[i].headline.main;
-            let by = results[i].byline.original;
-            // let url = results[i].web_url;
-            let resultDiv = $("<div>").addClass("result");
-            let titleRow = $("<div>").addClass("title-row");
-            let index = $("<div>").addClass("index").text(i + 1);
-            let title = $("<div>").addClass("title").text(headline);
-            let author = $("<div>").addClass("author").text(by);
-            titleRow.append(index);
-            titleRow.append(title);
-            resultDiv.append(titleRow);
-            resultDiv.append(author);
-            articleDiv.append(resultDiv);
+            buildResult(i, results[i], articleDiv);
         }
     });
+}
+
+// Create a single result group div and attach it to the specified div
+function buildResult(i, data, attachTo) {
+    let headline = data.headline.main;
+    let by = data.byline.original;
+    let url = data.web_url;
+    let resultDiv = $("<div>").addClass("result");
+    let titleRow = $("<div>").addClass("title-row");
+    let index = $("<div>").addClass("index").text(i + 1);
+    let title = $("<a>").addClass("title").text(headline).attr("href", url).attr("target","_blank");
+    let author = $("<div>").addClass("author").text(by);
+    titleRow.append(index);
+    titleRow.append(title);
+    resultDiv.append(titleRow);
+    resultDiv.append(author);
+    attachTo.append(resultDiv);
 }
