1ea1750808463a666b1b4c0fbe1e151e01df6a84
Z Naddaf <znaddaf@protonmail.com>
2019-12-20 13:24:43 -0600

    practice commit

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function setResults(searchTerm, numResults, startYear, endYear) {
     // Update the HTML with the result Data from
     let articleDiv = $("#articlesDiv");
     articleDiv.empty();
-    for (let i=0; i<results.length; i++) {
+    for (let i = 0; i < results.length; i++) {
         let resultDiv = $("<div>").addClass("result");
         let titleRow = $("<div>").addClass("title-row");
-        let index = $("<div>").addClass("index").text(i+1);
+        let index = $("<div>").addClass("index").text(i + 1);
         let title = $("<div>").addClass("title").text(results[i].headline);
         let author = $("<div>").addClass("author").text(results[i].author);
         titleRow.append(index);
